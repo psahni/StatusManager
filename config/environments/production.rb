9cf9af812f2eff1857723c7af323e8d3ab8cc2e1
Prashant Sahni <prashant.sahni5@gmail.com>
2014-12-04 15:43:23 +0530

    prod config updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ StatusManager::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -31,6 +31,7 @@ StatusManager::Application.configure do
 
   # Generate digests for assets URLs.
   config.assets.digest = true
+  config.assets.enabled = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
